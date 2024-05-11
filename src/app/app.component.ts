9eca4e3d57cf1fe9ecaca4c6eba156b75a34913f
Khang Tran <khangtrann8198@gmail.com>
2024-05-12 00:58:52 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, inject, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: `
+    <div style="
+      height: 300vh;
+      background-color: black;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    ">
+      <img #image width="700" />
+    </div>
+  `,
 })
-export class AppComponent {
-  title = 'video-scrolling';
+export class AppComponent implements OnInit {
+  renderer = inject(Renderer2);
+
+  image = viewChild<ElementRef<HTMLImageElement>>('image');
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // TODO: improve calculating for threshold
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const end = height / 3 * 2 + (285 / 2);
+    const threshold = (document.documentElement.clientHeight + 285) / 2;
+
+    if (winScroll > threshold && winScroll < end) {
+      const percent =  Math.round((winScroll - threshold) / (end - threshold) * 100);
+
+      if (percent > 0 && percent <= 86) {
+        this.renderer.setProperty(this.image()?.nativeElement, 'src', `images/${percent}.webp`);
+      }
+    }
+  }
+
+  ngOnInit(): void {
+  }
 }
